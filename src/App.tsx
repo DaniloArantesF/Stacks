e070c83a6c6872ad66e00e694ba334b3f6f82de8
DaniloArantesF <dfilho@purdue.edu>
2022-08-30 13:20:08 -0300

    code improvements + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,215 +1,257 @@
 import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useEffect, useMemo, useRef } from 'react';
-import create from 'zustand';
+import { useEffect, useMemo, useRef } from 'react'
+import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 import './App.css'
-import CameraDolly, { initialCameraPosition } from './CameraDolly';
-import { nanoid } from 'nanoid';
+import CameraDolly from './CameraDolly'
+import { nanoid } from 'nanoid'
 
 /** Constants */
-export const LAYER_HEIGHT = .5;
-const INIT_LAYER_SIDE = .5;
-const INIT_POSITION_X = { x: -2.5, z: 0 };
+export const LAYER_HEIGHT = 0.5
+const INIT_LAYER_SIDE = 0.5
+const INIT_POSITION_X = { x: -2.5, z: 0 }
 const INIT_POSITION_Z = { x: 0, z: -2.5 }
-const SPEED = 0.01;
+const SPEED = 0.01
+const WIREFRAME = false
+export const Y_OFFSET = INIT_LAYER_SIDE / 2
 
 /** Types */
 interface StackState {
-  layers: any[];
-  addLayer: (
-    x: number,
-    z: number,
-    width: number,
-    depth: number
-  ) => void;
-  nextDirection: 'x' | 'z'; // 0 -> move along x axis, 1 -> move along z axis
-  status: GameStatus,
-  setStatus: (status: GameStatus) => void;
-  reset: () => void;
+  layers: any[]
+  addLayer: (x: number, z: number, width: number, depth: number) => void
+  nextDirection: 'x' | 'z' // 0 -> move along x axis, 1 -> move along z axis
+  status: GameStatus
+  setStatus: (status: GameStatus) => void
+  reset: () => void
 }
 
-type GameStatus = 'READY' | 'RUNNING' | 'OVER';
+type GameStatus = 'READY' | 'RUNNING' | 'OVER'
 
 /** Game State */
-const initialState: Pick<StackState, "status" | "layers" | "nextDirection"> = {
+const initialState: Pick<StackState, 'status' | 'layers' | 'nextDirection'> = {
   status: 'READY',
-  layers: [<Layer key={nanoid()} x={0} y={0} z={0} width={INIT_LAYER_SIDE} depth={INIT_LAYER_SIDE} />],
+  layers: [
+    <Layer
+      key={nanoid()}
+      x={0}
+      y={Y_OFFSET}
+      z={0}
+      width={INIT_LAYER_SIDE}
+      depth={INIT_LAYER_SIDE}
+    />,
+  ],
   nextDirection: 'x',
 }
 
 export const useStackStore = create<StackState>()(
-  devtools(
-    (set) => ({
-      ...initialState,
-      setStatus: (status: GameStatus) => set((state) => ({ status })),
-      addLayer: (x,z,width,depth) => set((state) => {
+  devtools((set) => ({
+    ...initialState,
+    setStatus: (status: GameStatus) => set((state) => ({ status })),
+    addLayer: (x, z, width, depth) =>
+      set((state) => {
         // Determine y position for new layer
-        const y = LAYER_HEIGHT * state.layers.length;
+        const y = LAYER_HEIGHT * state.layers.length + Y_OFFSET
         return {
-          layers: [...state.layers, <Layer key={nanoid()} x={x} y={y}  z={z} width={width} depth={depth} direction={state.nextDirection} />],
+          layers: [
+            ...state.layers,
+            <Layer
+              key={nanoid()}
+              x={x}
+              y={y}
+              z={z}
+              width={width}
+              depth={depth}
+              direction={state.nextDirection}
+            />,
+          ],
           nextDirection: state.nextDirection === 'x' ? 'z' : 'x',
           status: 'RUNNING',
         }
       }),
-      reset: () => set(() => ({...initialState}))
-    })
-  )
-);
+    reset: () => set(() => ({ ...initialState })),
+  }))
+)
 
 interface LayerProps {
-  x: number;
-  y: number;
-  z: number;
-  direction?: 'x' | 'z';
-  width: number;
-  depth: number;
+  x: number
+  y: number
+  z: number
+  direction?: 'x' | 'z'
+  width: number
+  depth: number
 }
 
-function Layer({x, y, z, width, depth, direction }: LayerProps) {
-  const ref = useRef<THREE.Mesh>(null);
-  const { layers, status, setStatus } = useStackStore();
-  const layerIndex = y / LAYER_HEIGHT;
-  const isLayerActive = useRef(true);
-  const activeMaterial = useMemo(() => new THREE.MeshLambertMaterial({ wireframe: true, color: 'green' }), []);
-  const inactiveMaterial = useMemo(() => new THREE.MeshLambertMaterial({ wireframe: true, color: 'red' }), []);
+function Layer({ x, y, z, width, depth, direction }: LayerProps) {
+  const ref = useRef<THREE.Mesh>(null)
+  const { layers, status, setStatus } = useStackStore()
+  const layerIndex = (y - Y_OFFSET) / LAYER_HEIGHT
+  const isLayerActive = useRef(true)
+  const activeMaterial = useMemo(
+    () =>
+      new THREE.MeshLambertMaterial({ wireframe: WIREFRAME, color: 'green' }),
+    []
+  )
+  const inactiveMaterial = useMemo(
+    () => new THREE.MeshLambertMaterial({ wireframe: WIREFRAME, color: 'red' }),
+    []
+  )
 
   // Run every time the stack changes
   // Assign materials
   useEffect(() => {
-    if (!ref.current || !isLayerActive.current || layers.length - layerIndex > 2) {
-      return;
+    if (
+      !ref.current ||
+      !isLayerActive.current ||
+      layers.length - layerIndex > 2
+    ) {
+      return
     }
-    isLayerActive.current = ((layerIndex > 0) && (layers.length - 1 === layerIndex));
+    isLayerActive.current = layerIndex > 0 && layers.length - 1 === layerIndex
 
     // dont set first layer as active
     if (isLayerActive.current) {
-      ref.current.material = activeMaterial;
+      ref.current.material = activeMaterial
     } else {
-      ref.current.material = inactiveMaterial;
+      ref.current.material = inactiveMaterial
     }
-  }, [layers]);
-
+  }, [layers])
 
   useFrame((state) => {
-    if (!ref.current || !isLayerActive.current || status === 'OVER') return;
+    if (!ref.current || !isLayerActive.current || status === 'OVER') return
 
     // Move layer along x or z axis
     // Set game over if coords go over limit
-    const position = ref.current.position;
-    const positionAxis = position[direction as keyof typeof position];
+    const position = ref.current.position
+    const positionAxis = position[direction as keyof typeof position]
 
     // Move blocks over axis until limit
     if (positionAxis < 5) {
-      position.setComponent(direction === 'x' ? 0 : 2, (positionAxis as number) + SPEED);
+      position.setComponent(
+        direction === 'x' ? 0 : 2,
+        (positionAxis as number) + SPEED
+      )
     } else {
       // update status only once
       if (status === 'RUNNING') {
-        setStatus('OVER');
+        setStatus('OVER')
       }
     }
-  });
+  })
 
   // Set first layer as inactive
   // maybe move this to the prep layer function
   useEffect(() => {
     if (ref.current && layerIndex === 0) {
-      ref.current.material = inactiveMaterial;
-      isLayerActive.current = false;
+      ref.current.material = inactiveMaterial
+      isLayerActive.current = false
     }
   }, [])
 
   return (
-    <mesh
-      ref={ref}
-      position={[x, y, z]}
-    >
+    <mesh ref={ref} position={[x, y, z]}>
       <boxGeometry args={[width, LAYER_HEIGHT, depth]} />
     </mesh>
   )
 }
 
 function App() {
-  const { reset, status, setStatus, layers, addLayer, nextDirection } = useStackStore();
-  const groupRef = useRef<THREE.Group>(null);
+  const { reset, status, setStatus, layers, addLayer, nextDirection } =
+    useStackStore()
+  const groupRef = useRef<THREE.Group>(null)
+
+  const aspect = useMemo(() => window.innerWidth / window.innerHeight, [])
+  const width = useMemo(() => 10, [])
+  const height = useMemo(() => width / aspect, [])
+  const cameraProps = useMemo(
+    () => ({
+      position: new THREE.Vector3(5, 2.5, 5),
+      left: width / -2,
+      right: width / 2,
+      top: height / 2,
+      bottom: height / -2,
+    }),
+    []
+  )
 
   const prepNewLayer = () => {
-    if (!groupRef.current || status === 'OVER') return;
+    if (!groupRef.current || status === 'OVER') return
     if (status === 'READY') {
       // First layer
-      const width = INIT_LAYER_SIDE;
-      const depth = INIT_LAYER_SIDE;
-      let x = 0, z = 0;
+      const width = INIT_LAYER_SIDE
+      const depth = INIT_LAYER_SIDE
+      let x = 0,
+        z = 0
       if (nextDirection === 'z') {
-        x = INIT_POSITION_Z.x, z = INIT_POSITION_Z.z;
+        ;(x = INIT_POSITION_Z.x), (z = INIT_POSITION_Z.z)
       } else {
-        x = INIT_POSITION_X.x, z = INIT_POSITION_X.z;
+        ;(x = INIT_POSITION_X.x), (z = INIT_POSITION_X.z)
       }
-      addLayer(x, z, width, depth);
+      addLayer(x, z, width, depth)
     } else {
       // Calculate intersection of current top with last position
-      const topLayer = groupRef.current.children[groupRef.current.children.length - 1];
-      const lastLayer = groupRef.current.children[groupRef.current.children.length - 2];
+      const topLayer =
+        groupRef.current.children[groupRef.current.children.length - 1]
+      const lastLayer =
+        groupRef.current.children[groupRef.current.children.length - 2]
 
-      const { width: topWidth, depth: topDepth } = layers[layers.length - 1].props;
+      const { width: topWidth, depth: topDepth } =
+        layers[layers.length - 1].props
 
-      let delta = 0;
-      const direction = nextDirection === 'x' ? 'z' : 'x';
+      let delta = 0
+      const direction = nextDirection === 'x' ? 'z' : 'x'
 
       if (direction === 'x') {
         // top layer is moving along x axis
-        delta = topLayer.position.x - lastLayer.position.x;
+        delta = topLayer.position.x - lastLayer.position.x
       } else {
         // top layer is moving along z axis
-        delta = topLayer.position.z - lastLayer.position.z;
+        delta = topLayer.position.z - lastLayer.position.z
       }
 
-      const size = direction === 'x' ? topWidth : topDepth;
-      const overhangSize = Math.abs(delta);
-      const overlap = size - overhangSize;
+      const size = direction === 'x' ? topWidth : topDepth
+      const overhangSize = Math.abs(delta)
+      const overlap = size - overhangSize
 
+      // TODO: set threshold to ignore really small gaps
       if (overlap > 0) {
-        const newWidth = direction === 'x' ? overlap : topWidth;
-        const newDepth = direction === 'z' ? overlap : topDepth;
+        const newWidth = direction === 'x' ? overlap : topWidth
+        const newDepth = direction === 'z' ? overlap : topDepth
 
         // Update model
-        if (direction === 'x') {
-          topLayer.scale.x = overlap / size;
-          topLayer.position.x -= delta / 2;
-        } else {
-          topLayer.scale.z = overlap / size;
-          topLayer.position.z -= delta / 2;
-        }
+        topLayer.scale.setComponent(direction === 'x' ? 0 : 2, overlap / size)
+        const newPosition =
+          topLayer.position.getComponent(direction === 'x' ? 0 : 2) - delta / 2
+        topLayer.position.setComponent(direction === 'x' ? 0 : 2, newPosition)
 
         // Justify x or z coords with last layer
-        const newX = direction === 'x' ? topLayer.position.x : INIT_POSITION_X.x;
-        const newZ = direction === 'z' ? topLayer.position.z : INIT_POSITION_Z.z;
+        const newX = direction === 'x' ? topLayer.position.x : INIT_POSITION_X.x
+        const newZ = direction === 'z' ? topLayer.position.z : INIT_POSITION_Z.z
 
         // Add new layer with newWidth and newDepth
-        addLayer(newX, newZ, newWidth, newDepth);
+        addLayer(newX, newZ, newWidth, newDepth)
       } else {
         // Game over, objects did not intersect
-        setStatus('OVER');
+        setStatus('OVER')
       }
     }
   }
 
   return (
     <>
-      <div className='controls'>
-        { status === 'OVER' && <h1 className='gameover'>GAME OVER</h1>}
+      <div className="controls">
+        {status === 'OVER' && <h1 className="gameover">Game Over</h1>}
         <button onClick={prepNewLayer}>Add Layer</button>
         <button onClick={reset}>Reset</button>
       </div>
-      <Canvas orthographic camera={{position: initialCameraPosition, zoom: 250,}}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+      <Canvas
+        orthographic
+        camera={{ ...cameraProps, near: 0, far: 100, zoom: 500 }}
+      >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 30, 10]} intensity={0.6} />
         <gridHelper />
-        <group ref={groupRef}>
-        {
-          layers.map((layer) => layer)
-        }
-        </group>
+        <group ref={groupRef}>{layers.map((layer) => layer)}</group>
         <CameraDolly />
       </Canvas>
     </>
--- src/CameraDolly.tsx
+++ src/CameraDolly.tsx
@@ -1,23 +1,23 @@
-import * as THREE from 'three';
-import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
-import { LAYER_HEIGHT, useStackStore } from "./App";
+import * as THREE from 'three'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useMemo, useRef } from 'react'
+import { LAYER_HEIGHT, useStackStore, Y_OFFSET } from './App'
 
-export const initialCameraPosition = new THREE.Vector3(5, 2.5, 5);
+export const initialCameraPosition = new THREE.Vector3(5, 2.5, 5)
 
 const CameraDolly = () => {
-  const { status, layers } = useStackStore();
-  const moved = useRef(false);
-  const { camera } = useThree();
+  const { status, layers } = useStackStore()
+  const moved = useRef(false)
+  const { camera } = useThree()
 
   useEffect(() => {
-    camera.lookAt(0, (LAYER_HEIGHT * layers.length) / 2, 0);
+    camera.lookAt(0, (LAYER_HEIGHT * layers.length + Y_OFFSET) / 2, 0)
   }, [])
 
   // Reset camera position
   useEffect(() => {
     if (camera.position.y > 1 && status === 'READY') {
-      camera.position.copy(initialCameraPosition);
+      camera.position.copy(initialCameraPosition)
     }
   }, [status])
 
@@ -26,23 +26,26 @@ const CameraDolly = () => {
   // iteration after a new layer is added
   useEffect(() => {
     if (moved.current) {
-      moved.current = false;
+      moved.current = false
     }
   }, [layers])
-
 
   useFrame(({ camera }) => {
     // Only move camera after a new layer is added
     if (status === 'RUNNING' && !moved.current) {
       // Move camera along y axis
-      const newPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-      newPosition.y += LAYER_HEIGHT * 2;
-      camera.position.lerp(newPosition, .5);
-      moved.current = true;
+      const newPosition = new THREE.Vector3(
+        camera.position.x,
+        camera.position.y,
+        camera.position.z
+      )
+      newPosition.y += LAYER_HEIGHT * 2
+      camera.position.lerp(newPosition, 0.5)
+      moved.current = true
     }
-  });
+  })
 
-  return null;
+  return null
 }
 
-export default CameraDolly;
+export default CameraDolly
